3fedd85c11da7ca4d25dd682248d5f74f0a8f824
wyz001373 <626995657@qq.com>
2021-04-05 23:14:06 +0800

    master

---
+++ 两数之和.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    let map = new Map();
+    for (let k = 0, len = nums.length; k < len; k++) {
+        if (map.has(target - nums[k])) {
+            return [map.get(target - nums[k]), k]
+        } else {
+            map.set(nums[k], i);
+        }
+    }
+
+    return [];
+};
